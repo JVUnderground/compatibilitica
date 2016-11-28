a1b687f299d74e850f045c2c20a5cc367f045255
João Carneiro <joao.carneiro@gmail.com>
2016-11-28 10:41:37 -0200

    settings modified

--- compatibilitica/settings.py
+++ compatibilitica/settings.py
@@ -62,9 +62,9 @@ url = urlparse.urlparse(os.environ["DATABASE_URL"])
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'taskbuster_db',
-        'USER': 'admin',
-        'PASSWORD': 'ad13lj08',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
         'HOST': url.hostname,
         'PORT': url.port,
     }
