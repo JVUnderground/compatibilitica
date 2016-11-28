4f4d38739aaf0f3625d0f565789cdac529ec5fb1
João Carneiro <joao.carneiro@gmail.com>
2016-11-28 10:07:32 -0200

    modified settings to use postgresql database

--- compatibilitica/settings.py
+++ compatibilitica/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'compatibilitica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taskbuster_db',
+        'USER': 'admin',
+        'PASSWORD': 'ad13lj08',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
